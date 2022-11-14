5bba1fe8a21180a877444da95c6cef3fed21c09f
Tasha <tashachang2@gmail.com>
2022-11-14 13:23:20 -0800

    created ingredient model and breakfast_ingredient joint table

---
+++ app/models/breakfast_ingredient.py
@@ -0,0 +1,6 @@
+from app import db
+
+class BreakfastIngredient(db.Model):
+    __tablename__ = "breakfast_ingredient"
+    breakfast_id = db.Column(db.Integer, db.ForeignKey('breakfast.id'), primary_key=True, nullable = True)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), primary_key=True, nullable = True)
---
+++ app/models/ingredient.py
@@ -0,0 +1,6 @@
+from app import db
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    breakfast_items = db.relationship('Breakfast', secondary='breakfast_ingredient', back_populates='ingredients')
